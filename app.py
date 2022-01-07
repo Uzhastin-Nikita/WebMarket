3620c1436d06a9e2835ebea31488d72a69734ea3
Uzhastin-Nikita <fosfor20@mail.ru>
2022-01-08 00:02:38 +0300

    feat: addition redirect

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 
@@ -9,6 +9,22 @@ def index():
 @app.route('/registration')
 def registration():
     return render_template("registration.html")
+
+@app.route('/autorize')
+def autorize():
+    return render_template("index_autorize.html")
+
+@app.route('/verify', methods = ['POST', 'GET'])
+def registration_verify():
+    if request.method == 'POST':
+        name = request.form['name']
+        return redirect("/registration")
+
+@app.route('/verify', methods = ['POST', 'GET'])
+def autorize_verify():
+    if request.method == 'POST':
+        name = request.form['name']
+        return redirect("/autorize")
 
 
 if __name__ == "__main__":
