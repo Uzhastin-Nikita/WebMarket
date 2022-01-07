5720a2c1b154361c618f2155e6ea53e424e76a8d
Uzhastin-Nikita <fosfor20@mail.ru>
2022-01-07 23:17:16 +0300

    feat: update reg form

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/registration')
+def registration():
+    return render_template("registration.html")
+
 
 if __name__ == "__main__":
     app.run()
